e6e2a1b6920f503538940d4b6609d1b7da56acd3
Andres Castañeda <andrescastanedaxy@gmail.com>
2023-01-31 19:10:12 -0500

    Pagination

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Return a tuple containing the start and end indexes
+'''
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    Return a tuple containing the start and end indexes
+    '''
+    return (page * page_size - page_size, page * page_size)
